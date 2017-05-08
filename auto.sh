26fb568ef90b0ba377a06a1454334c3c4c0829b9
emirDev0 <emirDev.vmp@gmail.com>
2017-05-08 10:03:19 +0000

    Delete auto.sh

--- auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=1
-while(true) do
-./matador.sh
-curl "https://api.telegram.org/botتوکن/sendmessage" -F "chat_id= id" -F "text=#NEWCRASH-#TelePCT-Reloaded-${COUNTER}-times"
-let COUNTER=COUNTER+1 
-done
